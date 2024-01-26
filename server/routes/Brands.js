679ff2b7e82d4ac0a12a6a99e8adba0ce263e348
hemant singh parmar <hemantsinghparmar@mgmtech.org>
2024-01-26 14:30:03 +0530

    removed server files

--- server/routes/Brands.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-undef */
-const express = require('express');
-const { fetchBrands, createBrand } = require('../controller/Brand.js');
-
-const router = express.Router();
-//  /brands is already added in base path
-router.get('/', fetchBrands).post('/', createBrand);
-
-exports.router = router;
--- server/routes/Categories.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-undef */
-const express = require('express');
-const { fetchCategories, createCategory } = require('../controller/Category');
-
-const router = express.Router();
-//  /categories is already added in base path
-router.get('/', fetchCategories).post('/',createCategory)
-
-exports.router = router;
